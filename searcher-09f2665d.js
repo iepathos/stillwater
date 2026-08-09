e387a820ae4f1aecd06094806e546fa9362c9969
iepathos <iepathos@users.noreply.github.com>
2026-08-09 23:04:42 +0000

    deploy: 6fdf90548feca72b52035a2429d4ba52addc5f0b

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a1b6c38a.js',
+                path_to_root + 'searchindex-d7f45c93.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
